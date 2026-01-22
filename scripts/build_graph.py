d71451f8f3cf54bb86341cd103cff57a7440bc39
Chunkang Zhang <54783844+Za-ck@users.noreply.github.com>
2026-01-22 13:27:28 +0800

    Minor Refactor

--- scripts/build_graph.py
+++ scripts/build_graph.py
@@ -10,7 +10,7 @@ sys.path.append(PROJ_DIR)
 
 from myrag.storage import JsonKVStorage, NanoVectorDBStorage, NetworkXStorage
 from myrag import MyRAG, QueryParam
-from myrag.llm import wechat_gpt_complete, hf_embedding, hf_model_complete
+from myrag.llm import openai_complete, hf_embedding, hf_model_complete, vllm_complete
 from myrag.utils import EmbeddingFunc, truncate_list_by_token_size
 from transformers import AutoModel, AutoTokenizer
 
@@ -20,14 +20,10 @@ storage_class = {
     "NetworkXStorage": NetworkXStorage
 }
 
-APP_ID = "xxx"
-APP_KEY = "xxx"
-LLM_MODEL_NAME = "Qwen2.5-7B-Instruct"
 
 DATA_DIR = os.path.join(PROJ_DIR, "data")
 os.environ['HF_HOME'] = os.path.join(PROJ_DIR, "cache/")
 os.environ['TRANSFORMERS_CACHE'] = os.path.join(PROJ_DIR, "cache/")
-
 
 def normalize_question_str(q_str, dataset_name):
     if dataset_name == "financial":
@@ -48,8 +44,8 @@ def rag_insert(working_dir, context):
     rag = MyRAG(
         working_dir=working_dir,
         embedding_func=dummy_embedding_func,
-        llm_model_func=hf_model_complete,
-        llm_model_name=os.path.join(PROJ_DIR, f"cache/{LLM_MODEL_NAME}"),
+        llm_model_func=openai_complete,
+        llm_model_name=LLM_MODEL_NAME,
         entity_vector_storage=None,
         relationship_vector_storage=None,
         chunk_vector_storage=None,
@@ -72,23 +68,27 @@ if __name__ == "__main__":
         default="general",
         help="The domain of the source texts.",
     )
+    argparser.add_argument(
+        "--llm_model_name",
+        type=str,
+        default="gpt-4o",
+        help="The LLM model name to use.",
+    )
     args = argparser.parse_args()
     model_name_or_path = os.path.join(PROJ_DIR, 'cache/bge-m3')
 
     tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
     embed_model = AutoModel.from_pretrained(model_name_or_path)
     embed_hidden_size = embed_model.config.hidden_size
-
+    LLM_MODEL_NAME = args.llm_model_name
     source_texts = args.source_texts
     domains = args.domains
-    BASE_WORKING_DIR = os.path.join(PROJ_DIR, f"data/domains/{domains}/{source_texts}")
+    BASE_WORKING_DIR = os.path.join(PROJ_DIR, f"data/{source_texts}/domains/{domains}")
 
-    # 创建基础目录
     if not os.path.exists(BASE_WORKING_DIR):
         os.makedirs(BASE_WORKING_DIR)
-
-    # 读取 JSON 文件
-    json_file_path = os.path.join(DATA_DIR, f"/{source_texts}.json")
+    print(BASE_WORKING_DIR)
+    json_file_path = os.path.join(BASE_WORKING_DIR, f"Long_unique_contexts.json")
 
     with open(json_file_path, "r", encoding="utf-8") as rf:
         data = json.load(rf)
@@ -108,7 +108,6 @@ if __name__ == "__main__":
         print(f"Processing item {idx}...")
         print(f"Context length: {len(context)} characters")
 
-        # 插入到 RAG
         rag_insert(working_dir, context)
 
         print(f"Finished processing item {idx}, saved to {working_dir}\n")
