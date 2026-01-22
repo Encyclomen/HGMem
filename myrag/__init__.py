488c27bc3d34dbcbc751e2b293b0283529cbc082
Chunkang Zhang <54783844+Za-ck@users.noreply.github.com>
2026-01-22 13:14:43 +0800

    minor refractor

--- myrag/__init__.py
+++ myrag/__init__.py
@@ -1,5 +1,5 @@
 from .myrag import MyRAG as MyRAG, QueryParam as QueryParam
 
-__version__ = "1.0.0"
-__author__ = "Chulun Zhou"
+__version__ = "1.2.0"
+__author__ = "Chulun Zhou, Chunkang Zhang"
 __url__ = ""
